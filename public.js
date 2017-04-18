a0fd1b3d8869dcb85076430634c635565710e43b
rainbowzhuh <rainbow.23@163.com>
2017-04-18 10:05:35 +0800

    update_22
    22

--- public.js
+++ public.js
@@ -24,16 +24,18 @@ function showCompanyRightsReserved(divClass){
 	$(window).load(function () {//页面加载
 		var winWidth = $(window).width();
 		var htmlHeight = $("."+divClass).height();//页面html高度
-		alert(winWidth + "**" + winHeight + "**"+htmlHeight);
 		if(winHeight - htmlHeight <= 0){
 			$(".contact_html_info").css("position","relative");
 		}
 		else{
 			$(".contact_html_info").removeAttr("style");
 		}
+		
+		if(winWidth > winHeight){
+		   winHeight = 0;
+		}
 	});
 	$(window).resize(function () {//手机屏幕旋转
-		window.scrollTo(0,0); 
 		if(winHeight < $(window).height()){
 		   winHeight = $(window).height();
 			alert(winHeight);
